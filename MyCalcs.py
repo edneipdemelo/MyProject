6725ca200d847428e9bcdeac7045fcc2bebebca1
Ednei P. de Melo <root@darkstar.eti.br>
2024-05-30 13:05:21 -0300

    De novo...

--- MyCalcs.py
+++ MyCalcs.py
@@ -5,3 +5,12 @@ Y = int(input("Digite outro número: "))
 
 SOMA = X + Y
 print("O resultado da operação é:", SOMA)
+
+SUBTRACAO = X - Y
+print("O resultado da subtração é:", SUBTRACAO)
+
+MULTIPLICACAO = X - Y
+print("O resultado da multiplicação é:", MULTIPLICACAO)
+
+DIVISAO = X / Y
+print("O resultado da divisão é:", DIVISAO)
