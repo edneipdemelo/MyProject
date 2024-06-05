95e8146b7a2ce5347c736793c5f4a03e74ce0328
Ednei P. de Melo <root@darkstar.eti.br>
2024-06-05 18:37:01 -0300

    Ajustes no código-fonte MyOldCoin...

--- MyOldCoin.py
+++ MyOldCoin.py
@@ -3,7 +3,8 @@ print("Digite o número referente ao estado da moeda:")
 print("1. Flor de cunho")
 print("2. Soberba")
 print("3. Muito bem conservada")
-print("0. Outros estados")
+print("4. Bem conservada")
+print("5. Outros estados")
 ESTADO = int(input())
 
 match ESTADO:
@@ -13,7 +14,9 @@ match ESTADO:
         print("Quase perfeita! Ofereço R$ 250.000,00!")
     case 3:
         print("Que ótimo! Posso dar uns R$ 100.000,00!")
-    case 0:
+    case 4:
+        print("Que bom. Aceitaria R$ 30.000,00?")
+    case 5:
         print("Desculpe, não aceito neste estado.")
     case _:
         print("Opção não reconhecida!")
