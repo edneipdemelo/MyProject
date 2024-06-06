548b18de5ed6018b238a20e1d688445a1879ba75
Ednei P. de Melo <root@darkstar.eti.br>
2024-06-06 12:22:07 -0300

    Mais exemplos foram adicionados...

---
+++ MyForLoop.py
@@ -0,0 +1,7 @@
+# Demonstração do uso da estrutura repetitiva...
+print("Digite o valor máximo desejado:")
+NUMERO = int(input())
+
+print("Segue, os números desejados:")
+for X in range (0, NUMERO):
+    print(X)
---
+++ MyNameWith.py
@@ -0,0 +1,7 @@
+# Demonstração do uso de estrutura repetitiva...
+print("Digite o nome desejado:")
+NOME = input()
+
+print("Vamos soletrar cada letra?")
+for X in NOME:
+    print(X)
