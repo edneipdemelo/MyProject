c4c6ba7aca0e9a42c34e676af4b499de8774053d
Ednei P. de Melo <root@darkstar.eti.br>
2024-06-13 12:04:10 -0300

    Demonstração de testes de mesa...

---
+++ MyTableTest.py
@@ -0,0 +1,8 @@
+# Demonstração de teste de mesa...
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+C = int(input("Digite o valor de C: "))
+TOTAL = A + B + C
+MEDIA = TOTAL / 3
+print("A soma dos valores é:", TOTAL)
+print("A média dos valores é:", MEDIA)
