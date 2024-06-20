eb75c317927fa084e6692645628a56a5a89ca1d6
Ednei P. de Melo <root@darkstar.eti.br>
2024-06-19 23:30:36 -0300

    Atualização de código...

--- MyListsAccess.py
+++ MyListsAccess.py
@@ -1,13 +1,16 @@
 # Demonstração de acesso a listas...
 
+print("Vou montar a marmita com 5 alimentos!")
 MARMITA = ["feijão", "arroz", "legumes", "salada", "carne"]
-print("Marmita com 5 alimentos!")
 print("Eis, a nossa recomendação:", MARMITA)
 
-for X in range(0, 5):
-    print(f'Digite o {X+1}o. item do cardápio:')
-    MARMITA[X] = input()
-
-print("A marmita montada foi:", MARMITA)
-print("O três primeiros itens foram:", MARMITA[0:3])
-print("O último item da marmita foi:", MARMITA[-1])
+RESPOSTA = input("Você quer montar uma marmita diferente (S/N)?")
+if RESPOSTA == "S":
+    for X in range(0, 5):
+        print(f'Digite o {X+1}o. item do cardápio:')
+        MARMITA[X] = input()
+    print("A marmita montada foi:", MARMITA)
+    print("O três primeiros itens foram:", MARMITA[0:3])
+    print("O último item da marmita foi:", MARMITA[-1])
+else:
+    print("Ok. Você decide...")
