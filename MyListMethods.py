6ac357d43570b753d2f7c9a131abbe65066dae00
Ednei P. de Melo <root@darkstar.eti.br>
2024-06-23 14:53:15 -0300

    Olá...

---
+++ MyListMethods.py
@@ -0,0 +1,6 @@
+# Demonstração de métodos em listas...
+NUMEROS = [1, 3, 2, 4, 5, 3, 3, 2, 5]
+PALAVRAS = ["olá", "alô", "hei", "uau", "ops"]
+
+print("Quantos 3tem na lista Números?", NUMEROS.count(3))
+print("Quantos olá tem na lista Palavras?", PALAVRAS.count("alô"))
