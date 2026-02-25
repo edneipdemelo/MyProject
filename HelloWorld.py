f1e3743f6c2f7739761ec58a32aea020c29c7c0f
Ednei P. de Melo <root@darkstar.eti.br>
2026-02-25 19:33:40 -0300

    Demonstração do HelloWorld e uso de variáveis... - Foram usadas as funções print() e input(). - Além de converter os tipos com str, int e float.

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+# Demonstração de código em Python
+print("Olá Mundo!")
+
+print("Digite o seu nome:")
+NOME = input()
+print("Digite a sua altura:")
+ALTURA = float(input())
+print("Digite a sua idade:")
+IDADE = int(input())
+
+print("Meu nome é", NOME)
+print("A minha altura é", ALTURA)
+print("A minha idade é", IDADE)
