2d3b6cdb423a565d7d08c1da68439751f13999e8
Ednei P. de Melo <root@darkstar.eti.br>
2026-02-25 21:40:52 -0300

    Demonstração de operações matemáticas simples!

---
+++ Mate1.py
@@ -0,0 +1,8 @@
+# Solução do exercício 1, aula 3
+X = int(input("Digite o valor de X: "))
+Y = int(input("Digite o valor de Y: "))
+
+R = X + 5
+print("O resultado #1 é: ", R)
+R = Y - 3
+print("O resultado #2 é: ", R)
---
+++ Math.py
@@ -0,0 +1,7 @@
+# Demonstração de operações aritméticas...
+X = int(input("Digite o valor de X: "))
+Y = int(input("Digite o valor de Y: "))
+
+DIVISAO = X / Y
+print("O valor da divisão é:", DIVISAO)
+print(type(DIVISAO))
