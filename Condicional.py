60f82f9be33a161078301b1c32b87c74122258ae
Ednei P. de Melo <root@darkstar.eti.br>
2026-03-02 21:42:26 -0300

    Demonstração de condicionais...

---
+++ Condicional.py
@@ -0,0 +1,9 @@
+# Demonstração de Condicional...
+JOGO = input("Você vai assistir o jogo do Flamengo?")
+
+if JOGO.lower() == "sim":
+    print("Sim, sou mengão de coração!")
+else:
+    print("Não, torço para outro time...")
+
+print("De qualquer forma, metade dos torcedores do Rio ficarão felizes...")
